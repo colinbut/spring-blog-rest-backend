ae62609ae2499a2738756b602529c2d196b7927e
Colin But <colinbut@users.noreply.github.com>
2016-11-23 16:12:54 +0000

    cleaned up maven template

--- src/main/java/com/mycompany/blog/App.java
+++ src/main/java/com/mycompany/blog/App.java
@@ -1,13 +1,11 @@
 package com.mycompany.blog;
 
 /**
- * Hello world!
- *
+ * Application
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Application");
     }
 }
