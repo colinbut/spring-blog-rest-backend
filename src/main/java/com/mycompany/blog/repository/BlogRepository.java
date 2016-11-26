8fbfe230b6c0b638f70139c3fa77f017656215c4
Colin But <colinbut@users.noreply.github.com>
2016-11-26 19:32:25 +0000

    removed repository annotation from repository interface

--- src/main/java/com/mycompany/blog/repository/BlogRepository.java
+++ src/main/java/com/mycompany/blog/repository/BlogRepository.java
@@ -9,7 +9,6 @@ import com.mycompany.blog.model.BlogPost;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BlogRepository extends MongoRepository<BlogPost, String> {
 
     BlogPost findByBlogId(Integer blogId);
